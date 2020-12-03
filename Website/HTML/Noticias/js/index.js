fba80abbf44cbbb83b5a988afc6be5223b3b1b19
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-12-03 14:24:47 -0300

    AJAX

--- Website/HTML/Noticias/js/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = initPage;
-
-function initPage(){
-    $.post("../Website/Pages/index.aspx",null,function(Ciocollata){
-        console.log(Ciocollata);
-
-    });
-
-}
---
+++ Website/Website/index.js
@@ -0,0 +1,10 @@
+window.onload = initPage;
+
+function initPage(){
+    console.log("DIO");
+ //   $.post("../Website/Pages/index.aspx",null,function(Ciocollata){
+    //    console.log(Ciocollata);
+
+  //  });
+
+}
