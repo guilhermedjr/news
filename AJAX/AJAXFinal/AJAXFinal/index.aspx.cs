49310deabec333a8c863f52091fd9b4ec9a23571
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-12-03 14:44:48 -0300

    AJAX

---
+++ AJAX/AJAXFinal/AJAXFinal/index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data;
+
+namespace AJAXFinal
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataBase Classe = new DataBase();
+            Classe.openBar("localhost", "root", "root", "prjNoticias");
+            Classe.getCommand("SELECT * FROM noticia");
+            string Boku = "";
+            while (Classe.Selected.Read())
+            {
+                Boku += Classe.Selected["cd_noticia"] + "☺" + Classe.Selected["nm_titulo"] + "☺" + Classe.Selected["ds_noticia"] + "☺" + Classe.Selected["cd_categoria"] + "☻";
+
+            }
+
+            Response.Write(Boku);
+
+        }
+    }
+}
---
+++ AJAX/AJAXFinal/AJAXFinal/johannsebastian/index.js
@@ -0,0 +1,10 @@
+﻿window.onload = initPage;
+
+function initPage() {
+    console.log("DIO");
+       $.post("index.aspx",null,function(Ciocollata){
+        console.log(Ciocollata);
+
+      });
+
+}
