914f3b2a5b696c2132f82e769cd4d4271a1546fc
Tchaikochan <68945247+Tchaikochan@users.noreply.github.com>
2020-12-07 17:40:25 -0300

    AJAX

--- AJAX/AJAXFinal/AJAXFinal/johannsebastian/accept.js
+++ AJAX/AJAXFinal/AJAXFinal/johannsebastian/accept.js
@@ -1,9 +1,32 @@
 window.onload = initPage;
 
 function initPage(){
-    $.post("index.aspx",null,function(Ciocollata){
-        
-    
+    let Confirm = "true";
+    $.post("log.aspx",{c:Confirm},function(Ciocollata){
+        let MySpace = document.querySelector("#Add");
+        let Noticer = Ciocollata.split('☻');
+        Noticer.pop();
+        Noticer.forEach(element => {
+            Separar = element.split('☺');
+            MySpace.innerHTML += 
+            `
+            <h3>${Separar[1]}</h3>
+            <p>${Separar[2]}</p>
+            <button onclick="Took(${Separar[0]},1)">Aceitar</button>
+            <button onclick="Took(${Separar[0]},0)">Recusar</button>
+            `;
+
+        });
+
+    });
+
+}
+
+function Took(Code, Vein) {
+    let Confirm = "false";
+    $.post("log.aspx",{c:Confirm, Co:Code, Ve:Vein},function(Ciocollata){
+        document.location.reload(true);
+
     });
 
 }
--- AJAX/AJAXFinal/AJAXFinal/log.aspx.cs
+++ AJAX/AJAXFinal/AJAXFinal/log.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data;
 
 namespace AJAXFinal
 {
@@ -11,6 +12,47 @@ namespace AJAXFinal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string Boku = "";
+            string Code = Request["c"];
+            string CodeV = Request["Ve"];
+            string CodeG = Request["Co"]; 
+            DataBase Classe = new DataBase();
+            if (Code == "true")
+            {
+                Classe.openBar("localhost", "root", "root", "prjNoticias");
+                Classe.getCommand("SELECT * FROM noticia WHERE cd_situacao_noticia = 1");
+                while (Classe.Selected.Read())
+                {
+                    Boku += Classe.Selected["cd_noticia"] + "☺" + Classe.Selected["nm_titulo"] + "☺" + Classe.Selected["ds_noticia"] + "☺" + Classe.Selected["cd_categoria"] + "☻";
+
+                }
+
+                Classe.Refresh();
+                Response.Write(Boku);
+
+            }
+            else
+            {
+                if (CodeV == "0")
+                {
+                    Classe.openBar("localhost", "root", "root", "prjNoticias");
+                    Classe.setCommand("UPDATE noticia SET cd_situacao_noticia = 3 WHERE cd_noticia = " + CodeG);
+                    Classe.Refresh();
+
+                }
+                else
+                {
+                    Classe.openBar("localhost", "root", "root", "prjNoticias");
+                    Classe.setCommand("UPDATE noticia SET cd_situacao_noticia = 1 WHERE cd_noticia = " + CodeG);
+                    Classe.setCommand("UPDATE noticia SET dt_publicacao = CURRENT_DATE() WHERE cd_noticia = " + CodeG);
+                    Classe.setCommand("UPDATE noticia SET hr_publicacao = CURRENT_TIME() WHERE cd_noticia = " + CodeG);
+                    Classe.Refresh();
+
+                }
+
+
+            }
+
 
         }
     }
